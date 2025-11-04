decb4a0e61544070c7e201de6a047ca08bbb997c
hmontes <hmontes.dev@gmail.com>
2025-11-04 13:14:11 +0100

    Update index

--- app/src/main/java/com/example/filmsmanager/MainActivity.java
+++ app/src/main/java/com/example/filmsmanager/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.filmsmanager;
 
+import androidx. appcompat. app. ActionBar;
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx. appcompat. widget. Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+	RecyclerView recyclerView;
+	Toolbar toolbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,33 @@ public class MainActivity extends AppCompatActivity {
 			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 			return insets;
 		});
+		toolbar = findViewById(R.id.toolbar);
+		ActionBar actionBar = getSupportActionBar();
+		setSupportActionBar(toolbar);
+		recyclerView = findViewById(R.id.rv);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.menu_principal, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.all) {
+			return true;
+		} else if (id == R.id.add) {
+			return true;
+		} else if (id == R.id.see) {
+			return true;
+		} else if (id == R.id.show) {
+			return true;
+		}   else if (id == R.id.favs) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
