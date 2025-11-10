566935de24ef01a482b2899a07f861b7e25a258d
hmontes <hmontes.dev@gmail.com>
2025-11-10 09:56:16 +0100

    feat(threads): add adpaters

---
+++ app/src/main/java/com/example/filmsmanager/AdapterListadoCompleto.java
@@ -0,0 +1,13 @@
+package com.example.filmsmanager;
+
+import com.example.filmsmanager.resources.Pelicula;
+
+import java.util.ArrayList;
+
+public class AdapterListadoCompleto {
+	ArrayList<Pelicula> peliculas;
+
+	public AdapterListadoCompleto(ArrayList<Pelicula> peliculas){
+		this.peliculas = peliculas;
+	}
+}
