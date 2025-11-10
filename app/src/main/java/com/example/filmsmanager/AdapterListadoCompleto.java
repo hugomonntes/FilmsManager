fe034aaf10efc279ad544f8c8622d63c58d02866
hmontes <hmontes.dev@gmail.com>
2025-11-10 10:10:15 +0100

    feat(threads): add adpaters

--- app/src/main/java/com/example/filmsmanager/AdapterListadoCompleto.java
+++ app/src/main/java/com/example/filmsmanager/AdapterListadoCompleto.java
@@ -1,5 +1,13 @@
 package com.example.filmsmanager;
 
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.filmsmanager.resources.Pelicula;
 
 import java.util.ArrayList;
@@ -9,5 +17,19 @@ public class AdapterListadoCompleto {
 
 	public AdapterListadoCompleto(ArrayList<Pelicula> peliculas){
 		this.peliculas = peliculas;
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder {
+		private ImageView imgPortada;
+		private ImageView imgPegi;
+		private ImageButton imgButton;
+		private TextView txv4;
+		private TextView txv5;
+		private TextView txv6;
+		private TextView txv7;
+
+		public ViewHolder(@NonNull View itemView) {
+			super(itemView);
+		}
 	}
 }
